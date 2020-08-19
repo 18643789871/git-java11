664397b5a1446ac80b428927df9c939b2b00a031
18643789871 <596461793@qq.com>
2020-08-19 15:45:28 +0800

    项目1

---
+++ gitstudy/src/homework01/Student.java
@@ -0,0 +1,23 @@
+package homework01;
+
+public class Student {
+	private String name;
+	private int no;
+	private int score;
+
+	public Student(String name, int no, int score) {
+		super();
+		this.name = name;
+		this.no = no;
+		this.score = score;
+	}
+	
+	public void sort(Student[] students){
+		//循环数组，拿到每个对象里的成绩，成绩比大小
+		int[] array={99,77,66,88};
+		int len=array.length;
+		
+	}
+
+
+}
